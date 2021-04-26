0a0329039ce39d7701790249f4398aa69b5e8934
Gordon Craig <gordon.craig@pipware.net>
2021-04-26 16:28:05 +1000

    Add useList and useObject hooks

---
+++ src/useList.ts
@@ -0,0 +1,64 @@
+﻿import { useState, useMemo } from 'react';
+
+export interface ListMutator<T> {
+    set: (newList: T[]) => void;
+    push: (...items: T[]) => void;
+    update: (predicate: ListPredicate<T>, action: UpdateAction<T>) => void;
+    updateAt: (index: number, action: UpdateAction<T>) => void;
+    replace: (predicate: ListPredicate<T>, newItem: T) => void;
+    replaceAt: (index: number, newItem: T) => void;
+    remove: (predicate: ListPredicate<T>) => void;
+    removeAt: (index: number) => void;
+    clear: () => void;
+}
+
+export type ListPredicate<T> = (item: T) => boolean;
+export type UpdateAction<T> = (draftItem: T) => T;
+
+export function useList<T>(initialState: T[] = []): [T[], ListMutator<T>] {
+    const [list, setList] = useState<T[]>(initialState);
+
+    const mutator = useMemo<ListMutator<T>>(() => {
+        return {
+            set: (newList: T[]) => {
+                setList(newList);
+            },
+
+            push: (...items: T[]) => {
+                if (items.length === 0) return;
+
+                setList(prevList => [...prevList, ...items]);
+            },
+
+            update: (predicate: ListPredicate<T>, action: UpdateAction<T>) => {
+                setList(prevList => prevList.map(item => predicate(item) ? action({ ...item }) : item));
+            },
+
+            updateAt: (index: number, action: UpdateAction<T>) => {
+                setList(prevList => prevList.map((item, itemIndex) => itemIndex === index ? action({ ...item }) : item));
+            },
+
+            replace: (predicate: ListPredicate<T>, newItem: T) => {
+                setList(prevList => prevList.map(item => predicate(item) ? newItem : item));
+            },
+
+            replaceAt: (index: number, newItem: T) => {
+                setList(prevList => prevList.map((item, itemIndex) => itemIndex === index ? newItem : item));
+            },
+
+            remove: (predicate: ListPredicate<T>) => {
+                setList(prevList => prevList.filter(item => !predicate(item)));
+            },
+
+            removeAt: (index: number) => {
+                setList(prevList => prevList.splice(index, 1));
+            },
+
+            clear: () => {
+                setList([]);
+            }
+        };
+    }, [setList]);
+
+    return [list, mutator];
+}
---
+++ src/useObject.ts
@@ -0,0 +1,27 @@
+﻿import { useState, useMemo } from 'react';
+
+export interface ObjectMutator<T> {
+    set: (newObject: T) => void;
+    update: <K extends keyof T>(key: K, value: T[K]) => void;
+}
+
+export function useObject<T>(initialState: T = {} as T): [T, ObjectMutator<T>] {
+    const [state, setState] = useState<T>(initialState);
+
+    const mutator = useMemo<ObjectMutator<T>>(() => {
+        return {
+            set: (newObject: T) => {
+                setState(newObject);
+            },
+
+            update: (key, value) => {
+                setState(prevState => ({
+                    ...prevState,
+                    [key]: value
+                }));
+            }
+        };
+    }, [setState]);
+
+    return [state, mutator];
+}
